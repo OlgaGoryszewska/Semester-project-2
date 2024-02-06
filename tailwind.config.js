6af0cf6c3b822d64035cd7739783ff38af12f63d
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2024-02-06 18:03:01 +0100

    configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "#324C2A",
+      secondary: "#FF5C00",
+      secondary_light: "#FFB991",
+      tertiary: "#756A6D",
+      neutral: {
+        100: "#F0F0F0",
+        200: "#E2E0E0",
+      },
+    },
     extend: {},
   },
   plugins: [],
