f75079dbc65420fd9577c89cae6473aabdedaed9
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2024-02-27 16:28:32 +0100

    fetching items

--- public/js/listing/create.mjs
+++ public/js/listing/create.mjs
@@ -1,1 +1,18 @@
-export async function createItem(postData){}
+import { API_BASE_URL } from "../api/constants.mjs";
+import { authFetch } from "../api/authFetch.mjs";
+
+const action ="/auction/listings";
+const method= "post";
+
+export async function createItem(postData){
+    const createItemURL = API_BASE_URL + action;
+
+    const response = await authFetch(createItemURL, {
+        method,
+        body: JSON.stringify(postData)
+    })
+
+    const Item = await response.json();
+
+    console.log(post)
+}
